32b2fbdc32764ed4b6c610f8b4a6d6cb843c0823
bbandari27 <89539516+bbandari27@users.noreply.github.com>
2023-07-19 15:31:26 -0400

    Update cloudtrail.py

--- AWS/cloudtrail.py
+++ AWS/cloudtrail.py
@@ -41,18 +41,38 @@ def check_and_disable_cloudtrail(session, trail):
     else:
         print(f"'{trail_name}' CloudTrail is already disabled.")
 
+        print(f"Disabling '{trail_name}' CloudTrail in region {trail_region}...")
+
+def list_all_accounts_for_parent(org_client, parent_id):
+    all_accounts = []
+    next_token = None
+
+    while True:
+        if next_token:
+            response = org_client.list_accounts_for_parent(ParentId=parent_id, NextToken=next_token)
+        else:
+            response = org_client.list_accounts_for_parent(ParentId=parent_id)
+
+        all_accounts.extend(response['Accounts'])
+        next_token = response.get('NextToken')
+        if not next_token:
+            break
+
+    return all_accounts
+
 control_tower_role_name = 'AWSControlTowerExecution'
 ou_ids = ['ou1', 'ou2', 'ou3', 'ou4']
 
 for ou_id in ou_ids:
     print(f"Checking accounts in OU {ou_id}...")
     org_client = boto3.client('organizations')
-    accounts = org_client.list_accounts_for_parent(ParentId=ou_id)['Accounts']
+    accounts = list_all_accounts_for_parent(org_client, ou_id)
 
+    print (f"Number of accounts in OU {ou_id}: {len(accounts)}")
     for account in accounts:
         account_id = account['Id']
         #skip if the accounts is Raise Labs Inc
-        if account_id == 'accountdead':
+        if account_id == '030728503398':
             print(f"Skipping processing Raise Labs Inc Account, ID: {account_id}")
             continue
         #process remaining accounts 
