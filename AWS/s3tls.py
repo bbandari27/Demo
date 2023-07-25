708c75a16ef171e1374c428094fd39c316904eb5
bbandari27 <89539516+bbandari27@users.noreply.github.com>
2023-07-25 00:14:01 -0400

    Update s3tls.py

--- AWS/s3tls.py
+++ AWS/s3tls.py
@@ -1,27 +1,32 @@
 import boto3
-from botocore.exceptions import ProfileNotFound
+from botocore.exceptions import ClientError
 
 def is_tls_enabled(bucket_name, s3_client):
     try:
-        bucket_policy = s3_client.get_bucket_policy(Bucket=bucket_name)
-
-        # Check if the bucket policy includes a statement requiring TLS
-        for statement in bucket_policy['Policy']['Statement']:
-            if (
-                'Action' in statement and 's3:*' in statement['Action'] and
-                'Condition' in statement and 'Bool' in statement['Condition'] and
-                'aws:SecureTransport' in statement['Condition']['Bool'] and
-                statement['Condition']['Bool']['aws:SecureTransport'] == 'false'
-            ):
-                return False
+        bucket_policy_status = s3_client.get_bucket_policy_status(Bucket=bucket_name)
+        policy_status = bucket_policy_status['PolicyStatus']
+        if not policy_status.get('IsPublic'):
+            # Check if the bucket policy includes a statement requiring TLS
+            for statement in policy_status['Policy']['Statement']:
+                if (
+                    'Action' in statement and 's3:*' in statement['Action'] and
+                    'Condition' in statement and 'Bool' in statement['Condition'] and
+                    'aws:SecureTransport' in statement['Condition']['Bool'] and
+                    statement['Condition']['Bool']['aws:SecureTransport'] == 'false'
+                ):
+                    return False
 
         return True
-    except s3_client.exceptions.NoSuchBucketPolicy:
-        return False
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchBucketPolicy':
+            return False
+        else:
+            raise e
 
 def main():
     try:
-        session = boto3.Session(profile_name='YOUR_SSO_PROFILE_NAME')
+        session = boto3.Session()
         s3_client = session.client('s3')
 
         # List buckets
@@ -36,8 +41,8 @@ def main():
             else:
                 print(f"TLS is NOT enabled on {bucket_name}.")
 
-    except ProfileNotFound as e:
-        print(f"Error: {e}. Check if the SSO profile is configured correctly.")
+    except Exception as e:
+        print(f"Error: {e}. Check your AWS credentials configuration.")
 
 if __name__ == "__main__":
     main()
